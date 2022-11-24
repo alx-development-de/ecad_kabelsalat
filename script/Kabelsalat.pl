042784b7e75f08c885b31f56d9e8ed3aa2d24604
Alexander Thiel <alex@thiel-online.net>
2022-11-24 18:58:35 +0100

    Smaller bugfixes and code restructure

--- script/Kabelsalat.pl
+++ script/Kabelsalat.pl
@@ -26,18 +26,18 @@ GetOptions(
 
 # Initializing the logging mechanism
 Log::Log4perl->easy_init(Log::Log4perl::Level::to_priority(uc($log_level)));
-my $logger = Log::Log4perl->get_logger();
+my $logger = Log::Log4perl::get_logger();
 
 # Postprocessing command line parameters
 $opt_wiring_reference = File::Spec->rel2abs($opt_wiring_reference);
 $opt_device_reference = File::Spec->rel2abs($opt_device_reference);
-$opt_output_reference = File::Spec->rel2abs($opt_output_reference) if defined $opt_output_reference;
+$opt_output_reference = File::Spec->rel2abs($opt_output_reference) if (defined $opt_output_reference);
 
 # Checking if the required files are available
 if (-f $opt_wiring_reference) {$logger->debug("Wiring input file [$opt_wiring_reference]");}
-else {$logger->error("Wiring input file [$opt_wiring_reference] not available");}
+else {$logger->logdie("Wiring input file [$opt_wiring_reference] not available");}
 if (-f $opt_device_reference) {$logger->debug("Device input file [$opt_device_reference]");}
-else {$logger->error("Device input file [$opt_device_reference] not available");}
+else {$logger->logdie("Device input file [$opt_device_reference] not available");}
 if (defined $opt_output_reference) {$logger->debug("Output file [$opt_output_reference]");}
 
 $logger->debug("Analyzing device list content");
@@ -45,25 +45,23 @@ my %device_structure = ();
 my @device_file_content = File::Slurp::read_file($opt_device_reference, { 'chomp' => 1 });
 foreach my $line (@device_file_content) {
     #chomp($line);
-    my ($device, $articlestring, $master) = split(/[;]/, $line);
+    my ($device, $article_name, $master) = split(/[;]/, $line);
 
     # Checking device identifier against the EN81346 specification
     $logger->warn("Device identifier [$device] is not valid according EN81346") unless ALX::EN81346::is_valid($device);
 
-    $logger->debug("Parsed line content: [$line] results in DEVICE: [$device] MASTER: [$master] ARTICLES: [$articlestring]");
-    $device_structure{$device}{'MASTER'} = $master if ($master);
+    $logger->debug("Parsed line content: [$line] results in DEVICE: [$device] MASTER: [$master] ARTICLES: [$article_name]");
+    $device_structure{$device}{'MASTER'} = $master if (defined $master);
 
-    my @articles = split(/,/, $articlestring);
+    my @articles = split(/,/, $article_name);
     if (scalar(@articles)) {
         $logger->debug("Parsed article structure: [" . join("] [", @articles) . "]");
         $device_structure{$device}{'ARTICLES'} = \@articles;
     }
 }
 
-# A Device structure has been build from file
-print Dumper \%device_structure;
-
 # Building a article list depending on the articles found in the structure
+# TODO: Check if the BOM is really required. Seems to be not needed
 my %bom = ();
 foreach my $device (keys(%device_structure)) {
     foreach my $article (@{$device_structure{$device}{'ARTICLES'}}) {
@@ -71,16 +69,15 @@ foreach my $device (keys(%device_structure)) {
     }
 }
 $logger->info(scalar(keys(%bom)), " Different article(s) found in the structure");
-print join(', ', keys(%bom)) . "\n";
+
+# TODO: Remove if not longer required
+# print Dumper \%device_structure; # Printing the device structure
+# print join(', ', keys(%bom)) . "\n"; # Printing all articles identified
 
 # Parsing the wiring information list
-open(WIRING, '<' . $opt_wiring_reference) or $logger->logdie("Failed to open wiring file [$opt_wiring_reference]");
-my @wiring_file_content = <WIRING>;
-close(WIRING);
-
+my @wiring_file_content = File::Slurp::read_file($opt_wiring_reference, { 'chomp' => 1 });
 foreach my $line (@wiring_file_content) {
-    chomp($line);
-    next unless $line; # skip empty ines
-    #    print "$line\n";
+    #next unless $line; # skip empty ines
+    print "$line\n";
 }
 
